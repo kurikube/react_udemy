edb8e1b97d7f867d151adea447677891e1852151
kurikube <nakamura.rikuto.0024390@gmail.com>
2023-01-28 20:44:56 +0900

    論理演算子

--- src/index.js
+++ src/index.js
@@ -193,20 +193,38 @@
  * 三項演算子
  */
 // ある条件 ? 条件がtrueの時: 条件がfalseの時
-const val1 = 1 < 0 ? 'trueです' : 'falseです';
-console.log(val1);
+// const val1 = 1 < 0 ? 'trueです' : 'falseです';
+// console.log(val1);
 
-const num = 1300;
-const str = "1300";
-console.log(num.toLocaleString());
-console.log(str.toLocaleString());
+// const num = 1300;
+// const str = "1300";
+// console.log(num.toLocaleString());
+// console.log(str.toLocaleString());
 
-const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
-const formattedStr = typeof str === 'number' ? str.toLocaleString() : '数値を入力してください';
-console.log(formattedNum);
-console.log(formattedStr);
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
+// const formattedStr = typeof str === 'number' ? str.toLocaleString() : '数値を入力してください';
+// console.log(formattedNum);
+// console.log(formattedStr);
 
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? '100を超えています' : '許容範囲内です';
+}
+console.log(checkSum(50, 60));
+console.log(checkSum(50, 40));
 
+/**
+ * 論理演算子の本当の意味をしろう &&  ||
+ */
+
+const flag1 = true;
+const flag2 = false;
+
+if (flag1 || flag2){
+  console.log("1か2はtrueになります");
+}
+if (flag1 && flag2){
+  console.log("1も2もtrueになります");
+}
 
 
 
