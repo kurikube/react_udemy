6442e53c3b70c7e4bf8674f8190f004b9e1c2618
kurikube <nakamura.rikuto.0024390@gmail.com>
2023-01-28 19:56:12 +0900

    スプレッド構文を追加

--- src/index.js
+++ src/index.js
@@ -134,11 +134,29 @@ sayHello();
 // sumFunc(...arr1);
 
 //まとめる
-const arr2 = [1, 2, 3, 4, 5];
-const [num1, num2, ...arr3] = arr2;
-console.log(num1);
-console.log(num2);
-console.log(arr3);
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+console.log(arr6);
+
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
+
+const arr8 = arr4;
+console.log(arr8);
+
+arr8[0] = 100;
+console.log(arr4);
+console.log(arr8);
+
 
 
 
