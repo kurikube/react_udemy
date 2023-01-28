2b660d2215248e00f31151c44481021720f8686a
kurikube <nakamura.rikuto.0024390@gmail.com>
2023-01-28 19:43:46 +0900

    スプレッド構文を追加

--- src/index.js
+++ src/index.js
@@ -129,6 +129,12 @@ const arr1 = [1, 2];
 console.log(arr1);
 console.log(...arr1);
 
+const sumFunc = (num1, num2) => console.log(num1 + num2);
+sumFunc(arr1[0], arr1[1]);
+sumFunc(...arr1);
+
+
+
 
 
 
