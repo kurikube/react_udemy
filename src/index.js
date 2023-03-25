9952ba78dacb7055f5dd2b2893dab2f870f309e9
kurikube <nakamura.rikuto.0024390@gmail.com>
2023-03-25 13:04:39 +0900

    add complete&delete button

--- src/index.js
+++ src/index.js
@@ -12,7 +12,15 @@ const onClickAdd = () => {
     // liタグ生成
     const li = document.createElement("li");
     li.innerText = inputText;
-    
+
+    // button（完了）タグ生成
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "完了";
+
+    // button（削除）タグ生成
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "削除";
+
     // divタグの子要素に各要素を設定
     div.appendChild(li);
 
